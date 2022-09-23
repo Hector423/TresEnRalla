bd093df16e536f163a2dfa5bc50ef3ee095d0c15
Hector <hectorvallveg@gmail.com>
2022-09-23 12:20:21 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 
+import controlador.ope.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		char[][] mapa = new char[3][3];
-		String torn = "";
-		String historial = "";
 		
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				mapa[i][j] = '#';
-			}
-			System.out.println(mapa[i]);
-		}
-
 	}
 
 }
